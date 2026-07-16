8266fb72b8be999ef6512ee362983871f5ddf75d
Jerine Varghese <53529516+jerinev@users.noreply.github.com>
2026-07-16 01:30:32 -0700

    Update wsgi.py

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,7 +1,1 @@
-import os
-from alphavantage_mcp_server.server import serve_http
 
-if __name__ == "__main__":
-    # Pulls Render's assigned port variable or defaults to 10000
-    port = int(os.environ.get("PORT", 10000))
-    serve_http(host="0.0.0.0", port=port)
